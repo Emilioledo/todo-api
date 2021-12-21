dc74472edf700a1308b78dfdfd1aaef261e2a336
Emilio Fernandez Ledo <53414316+Emilioledo@users.noreply.github.com>
2021-12-21 15:26:32 -0300

    Delete auth-routes.js

--- src/routes/auth-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authControllers = require("../controllers/auth-controller");
-
-router.post("/signin", authControllers.signIn);
-
-router.post("/signup", authControllers.signUp);
-
-module.exports = router;
